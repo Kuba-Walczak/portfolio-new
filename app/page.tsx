7648cfc928637ad6e3900e51ef07c97b53132b41
Jacob <kubawalczak005@gmail.com>
2026-01-22 23:54:57 +0100

    ref: deprecated header

--- app/page.tsx
+++ app/page.tsx
@@ -6,12 +6,9 @@ import About from '@/components/About'
 export default function Home() {
   return (
     <div className="min-h-screen bg-background">
-      <Header />
-      <main>
         <Hero />
         <Projects />
         <About />
-      </main>
     </div>
   )
 }
