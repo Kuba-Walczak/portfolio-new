14a67fe6f6e1432a15c7d5ab4ddd2c27f2e97a54
Jacob <kubawalczak005@gmail.com>
2026-02-12 12:10:46 +0100

    feat: updated about section to match style of hero section

--- components/About/index.tsx
+++ components/About/index.tsx
@@ -1,10 +1,7 @@
 'use client'
 
-import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card'
 import { Separator } from '@/components/ui/separator'
 import Image from 'next/image'
-import { useState } from 'react'
-import { Handshake } from 'lucide-react'
 
 const skills = [
   { category: 'Frontend', items: ['React', 'Next.js', 'TypeScript', 'Tailwind CSS'] },
@@ -14,46 +11,41 @@ const skills = [
 
 export default function About() {
   return (
-    <section id="about" className="max-w-400 mx-auto">
+    <section
+    id="about"
+    className="mx-auto scroll-mt-20"
+    style={{ maxWidth: 'calc(100vh * 1.2)' }}>
       <div className="flex items-center justify-center gap-16 p-16">
       <div className="flex flex-col gap-6">
             <div className="flex items-center gap-8">
-            <h2 className="text-7xl font-bold text-muted-foreground">
+            <h2 className="text-7xl font-bold">
               Hi, I'm Jacob
             </h2>
-            <Handshake className="w-16 h-16 text-muted-foreground" />
+            <Image
+        src="https://PortfolioPullZone.b-cdn.net/pjait-logo.svg?t=2"
+        alt="About Me"
+        width={500}
+        height={500}
+        className="rounded-xl bg-white/5 border w-24 h-24 p-2"
+        />
             </div>
-            <p className="text-lg text-muted-foreground leading-relaxed">
+            <div className="flex flex-col gap-1">
+            <span className="font-mono text-lg tracking-widest uppercase text-muted-foreground">About Me</span>
+            <p className="text-2xl font-medium leading-relaxed text-justify">
             My name's Jacob and I'm a full-time CS student at PJAIT.
             Throughout my life I've tinkered with various forms of creative expression, such as video editing, music and animation.
             Ultimately what I've found to love most is creating visuals that rely on complex systems to create amazing experiences that don't just look cool, but work in a bigger environment and perform well.
             Thank you for checking out my website!
             </p>
-            <Separator />
-            <div className="flex items-center gap-4 border rounded-xl bg-white/5 backdrop-blur-sm p-4 w-fit">
-              <p className="text-3xl font-medium">
-                PJAIT, Computer Science
-                </p>
-              <Image
-              src="https://PortfolioPullZone.b-cdn.net/pjait-logo.svg?t=2"
-              alt="PJAIT Logo"
-              width={75}
-              height={75}
-              className="opacity-75"
-              />
             </div>
-            <div className="flex items-center gap-4 border rounded-xl bg-white/5 backdrop-blur-sm p-4 w-fit">
-              <p className="text-3xl font-medium">
-                Warsaw, Poland
-              </p>
-              <Image
-              src="https://PortfolioPullZone.b-cdn.net/poland-logo.svg?t=2"
-              alt="Poland Logo"
-              width={120}
-              height={120}
-              className="opacity-75 rounded-lg"
-              />
-            </div>
+            <div className="flex flex-col gap-1">
+            <span className="font-mono text-lg tracking-widest uppercase text-muted-foreground">Education</span>
+            <span className="text-2xl font-medium text-foreground leading-relaxed">Computer Science<span className="mx-2">•</span>PJAIT<br/>2024<span className="mx-2">→</span>Present</span>
+          </div>
+          <div className="flex flex-col gap-1">
+            <span className="font-mono text-lg tracking-widest uppercase text-muted-foreground">Location</span>
+            <span className="text-2xl font-medium text-foreground leading-relaxed">Warsaw<span className="mx-2">•</span>Poland</span>
+          </div>
           </div>
         <Image
         src="https://PortfolioPullZone.b-cdn.net/LandingPage/FaceForeground.webp"
