78ed35184a1170a5028f6c5bf068cf22afc37135
Jacob <kubawalczak005@gmail.com>
2026-02-25 17:22:04 +0100

    ref: changed the icons in the about section to be tooltip shadcn components

--- components/About/index.tsx
+++ components/About/index.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { Separator } from '@/components/ui/separator'
+import { Tooltip, TooltipContent, TooltipTrigger } from '@/components/ui/tooltip'
 import Image from 'next/image'
 
 const skills = [
@@ -17,21 +17,44 @@ export default function About() {
     style={{ maxWidth: 'calc(100vh * 1.2)' }}>
       <div className="flex items-center justify-center gap-16 p-16">
       <div className="flex flex-col gap-6">
-            <div className="flex items-center gap-8">
+            <div className="flex flex-col gap-4">
+            <div className="flex items-center gap-2">
+            <Tooltip>
+              <TooltipTrigger asChild>
+                <Image
+                  src="https://PortfolioPullZone.b-cdn.net/pjait-logo.svg?t=2"
+                  alt="PJAIT Logo"
+                  width={500}
+                  height={500}
+                  className="rounded-xl bg-white/5 border w-24 h-24 p-2"
+                />
+              </TooltipTrigger>
+              <TooltipContent>
+                <p>Student of the Polish-Japanese Academy of Information Technology</p>
+              </TooltipContent>
+            </Tooltip>
+            <Tooltip>
+              <TooltipTrigger asChild>
+                <Image
+                  src="https://PortfolioPullZone.b-cdn.net/mirai-logo.webp"
+                  alt="Mirai Logo"
+                  width={500}
+                  height={500}
+                  className="rounded-xl bg-white/5 border w-16 h-24 object-contain"
+                />
+              </TooltipTrigger>
+              <TooltipContent>
+                <p>Member of the PJAIT Student Council</p>
+              </TooltipContent>
+            </Tooltip>
+            </div>
             <h2 className="text-7xl font-bold">
               Hi, I'm Jacob
             </h2>
-            <Image
-        src="https://PortfolioPullZone.b-cdn.net/pjait-logo.svg?t=2"
-        alt="About Me"
-        width={500}
-        height={500}
-        className="rounded-xl bg-white/5 border w-24 h-24 p-2"
-        />
             </div>
             <div className="flex flex-col gap-1">
             <span className="font-mono text-lg tracking-widest uppercase text-muted-foreground">About Me</span>
-            <p className="text-2xl font-medium leading-relaxed text-justify">
+            <p className="text-2xl leading-relaxed text-justify">
             My name's Jacob and I'm a full-time CS student at PJAIT.
             Throughout my life I've tinkered with various forms of creative expression, such as video editing, music and animation.
             Ultimately what I've found to love most is creating visuals that rely on complex systems to create amazing experiences that don't just look cool, but work in a bigger environment and perform well.
@@ -40,11 +63,11 @@ export default function About() {
             </div>
             <div className="flex flex-col gap-1">
             <span className="font-mono text-lg tracking-widest uppercase text-muted-foreground">Education</span>
-            <span className="text-2xl font-medium text-foreground leading-relaxed">Computer Science<span className="mx-2">•</span>PJAIT<br/>2024<span className="mx-2">→</span>Present</span>
+            <span className="text-2xl text-foreground leading-relaxed">Computer Science<span className="mx-2">•</span>PJAIT<br/>2024<span className="mx-2">→</span>Present</span>
           </div>
           <div className="flex flex-col gap-1">
             <span className="font-mono text-lg tracking-widest uppercase text-muted-foreground">Location</span>
-            <span className="text-2xl font-medium text-foreground leading-relaxed">Warsaw<span className="mx-2">•</span>Poland</span>
+            <span className="text-2xl text-foreground leading-relaxed">Warsaw<span className="mx-2">•</span>Poland</span>
           </div>
           </div>
         <Image
@@ -52,7 +75,7 @@ export default function About() {
         alt="About Me"
         width={500}
         height={500}
-        className="rounded-2xl bg-white/5 border"
+        className="rounded-2xl bg-white/5 border backdrop-blur-xl"
         />
       </div>
     </section>
